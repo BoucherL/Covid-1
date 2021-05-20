e922869ed54d09712f9c18551bcf348696cd7670
BoucherL <70692929+BoucherL@users.noreply.github.com>
2021-05-20 11:31:23 +0200

    ajout test pouvoir

--- class/test/testUnitaire/testPouvoir.php
+++ class/test/testUnitaire/testPouvoir.php
@@ -1,8 +1,12 @@
-<?php   //include "testUnitaire/test/Pouvoir.php"//test de Boucher
+<?php   //include "../../Pouvoir.php";//test de Boucher
     
      echo '<div class="testUnitaire"><p>Test 1 creation pouvoir </p>';
 
-     $pouvoir = new Equipement($mabase);
-     $pouvoir = $pouvoir->createEquipementAleatoire();
+     $equipement = new Equipement($mabase);
+     $equipement = $equipement->createEquipementAleatoire();
+     echo "<p>le nom est : ".$equipement->getNom()." ";
+
+     $pouvoir = new Pouvoir($mabase);
+     echo "Obtention de la force"
 
 ?>
