730de46ae5233d934a5ab1f9d6f69575d99943ef
BoucherL <70692929+BoucherL@users.noreply.github.com>
2021-05-20 12:37:43 +0200

    testTypeperso

--- class/test/testUnitaire/testTypePersonnage.php
+++ class/test/testUnitaire/testTypePersonnage.php
@@ -1,9 +1,18 @@
 <?php 
     //test boucher
-
-
-
-
+    echo '<div class="testUnitaire"><p>Test</p>';
     
+    //$personnage = new TypePersonnage($mabase);
 
+    $personnage = new Personnage($mabase);
+    
+    $personnage->setEntiteById($idEntitePersonnage);
+
+    echo '<p>Le nom du perso est '.$personnage->getNom().'</p>';
+
+    echo "<p> le id est : ".$personnage = $personnage->getId()." </p>";
+
+    $typepersonnage = new TypePersonnage($mabase);
+
+    $typepersonnage->setTypePersonnageById(1);
 ?>
